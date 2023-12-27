9e2eb0c95226a55711af5f852e321a3937675b32
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-12-27 23:46:29 +0900

    feat: autoComplete 컴포넌트 생성, scrollbar 관련 tailwind 패키지 설치 및 설정

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,17 @@ import "./App.css";
 import axios from "axios";
 import { useState } from "react";
 import PokeCard from "./components/PokeCard";
-import { useDebounce } from "./hooks/useDebounce";
+import AutoComplete from "./components/AutoComplete";
 
 function App() {
   const [allPokemons, setAllpokemons] = useState([]);
   const [displayedPokemons, setDisplayedPokemons] = useState([]);
   const limitNum = 20;
   const url = `https://pokeapi.co/api/v2/pokemon?limit=1008&offset=0`;
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
   useEffect(() => {
     fetchPokeData();
   }, []);
-
-  useEffect(() => {
-    handleSearchInput(debouncedSearchTerm);
-  }, [debouncedSearchTerm]);
 
   const filterDisplayedPokemonData = (
     allPokemonsData,
@@ -43,45 +36,13 @@ function App() {
     }
   };
 
-  const handleSearchInput = async (searchTerm) => {
-    if (searchTerm.length > 0) {
-      try {
-        const response = await axios.get(
-          `https://pokeapi.co/api/v2/pokemon/${searchTerm}`
-        );
-        const pokemonData = {
-          url: `https://pokeapi.co/api/v2/pokemon/${response.data.id}`,
-          name: searchTerm,
-        };
-        setDisplayedPokemons([pokemonData]);
-      } catch (err) {
-        setDisplayedPokemons([]);
-        console.log(err);
-      }
-    } else {
-      fetchPokeData(true);
-    }
-  };
-
   return (
     <article className="pt-6">
       <header className="flex flex-col gap-2 w-full px-4 z-50">
-        <div className="relative z-50">
-          <form className="relative flex justify-center items-center w-[20.5rem] h-6 rounded-lg m-auto">
-            <input
-              type="text"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-              className="text-xs w-[20.5rem] h-6 px-2 py-1 rounded-lg bg-[hsl(214,13%,47%)] text-gray-300 text-center"
-            />
-            <button
-              type="submit"
-              className="text-xs bg-slate-900 text-slate-300 w-[2.5rem] h-6 px-2 py-1 rounded-r-lg text-center absolute right-0 hover:bg-slate-700"
-            >
-              검색
-            </button>
-          </form>
-        </div>
+        <AutoComplete
+          allPokemons={allPokemons}
+          setDisplayedPokemons={setDisplayedPokemons}
+        />
       </header>
       <section className="pt-6 flex flex-col justify-content items-center overflow-auto z-0">
         <div className="flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl">
