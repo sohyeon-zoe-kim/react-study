dd19b84b0d787b71f590f166542a8d798dfa9fcc
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-12-28 15:49:36 +0900

    feat: 포켓몬 상세 페이지 데이터 생성하기

--- src/pages/DetailPage/index.jsx
+++ src/pages/DetailPage/index.jsx
@@ -1,6 +1,44 @@
-import React from "react";
+import axios from "axios";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const DetailPage = () => {
+  const params = useParams();
+  const pokemonId = params.id;
+  const baseUrl = `https://pokeapi.co/api/v2/pokemon/`;
+
+  useEffect(() => {
+    fetchPokemonData();
+  }, []);
+
+  const fetchPokemonData = async () => {
+    const url = `${baseUrl}${pokemonId}`;
+    try {
+      const { data: pokemonData } = await axios.get(url);
+      if (pokemonData) {
+        const { name, id, types, weight, height, stats, ablities } =
+          pokemonData;
+        const nextAndPreviousPokemon = await getNextAndPreviousPokemon(id);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getNextAndPreviousPokemon = async (id) => {
+    const urlPokemon = `${baseUrl}?limit=1&offset=${id - 1}`;
+    const { data: pokemonData } = await axios.get(urlPokemon);
+    const nextResponse =
+      pokemonData.next && (await axios.get(pokemonData.next));
+    const previousResponse =
+      pokemonData.previous && (await axios.get(pokemonData.previous));
+
+    return {
+      next: nextResponse?.data?.results?.[0]?.name,
+      previous: previousResponse?.data?.results?.[0].name,
+    };
+  };
+
   return <div>DetailPage</div>;
 };
 
