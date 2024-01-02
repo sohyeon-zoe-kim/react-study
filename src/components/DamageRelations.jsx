617112243d80655d6b217f1e889936a28def84d1
sohyeon-zoe-kim <hlna030838@gmail.com>
2024-01-02 14:01:53 +0900

    feat: 타입이 두 개일 때 Damage Relation 데이터 생성하기

--- src/components/DamageRelations.jsx
+++ src/components/DamageRelations.jsx
@@ -1,17 +1,65 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const DamageRelations = ({ damages }) => {
+  const [damagePokemonForm, setDamagePokemonForm] = useState();
   useEffect(() => {
     const arrayDamage = damages.map((damage) =>
       separateObjectBetweenToAndFrom(damage)
     );
 
-    if (arrayDamage.length > 2) {
-      // 속성이 2개일 때
+    if (arrayDamage.length === 2) {
+      const obj = joinDamageRelations(arrayDamage);
+      setDamagePokemonForm(reduceDuplicateValues(postDamageValue(obj.from)));
     } else {
-      postDamageValue(arrayDamage[0].from);
+      setDamagePokemonForm(postDamageValue(arrayDamage[0].from));
     }
   }, []);
+
+  const joinObjects = (props, string) => {
+    const key = string;
+    const firstArrayValue = props[0][key];
+    const secondArrayValue = props[1][key];
+    const result = Object.entries(secondArrayValue).reduce(
+      (acc, [keyName, value]) => {
+        const result = firstArrayValue[keyName].concat(value);
+        return (acc = { ...acc, [keyName]: result });
+      },
+      {}
+    );
+    return result;
+  };
+
+  const joinDamageRelations = (props) => {
+    return {
+      to: joinObjects(props, "to"),
+      from: joinObjects(props, "from"),
+    };
+  };
+
+  const reduceDuplicateValues = (props) => {
+    const duplicateValues = {
+      double_damage: "4x",
+      half_damage: "1/4x",
+      no_damage: "0x",
+    };
+
+    return Object.entries(props).reduce((acc, [keyName, value]) => {
+      const key = keyName;
+      const verifiedValue = filterForUniqueValues(value, duplicateValues[key]);
+      return (acc = { [keyName]: verifiedValue, ...acc });
+    }, {});
+  };
+
+  const filterForUniqueValues = (valueForFiltering, damageValue) => {
+    return valueForFiltering.reduce((acc, currentValue) => {
+      const { url, name } = currentValue;
+      const filterAcc = acc.filter((a) => a.name !== name);
+
+      return filterAcc.length === acc.length
+        ? (acc = [currentValue, ...acc])
+        : (acc = [{ damageValue: damageValue, name, url }, ...acc]);
+    }, []);
+  };
 
   const postDamageValue = (props) => {
     const result = Object.entries(props).reduce((acc, [keyName, value]) => {
@@ -30,7 +78,7 @@ const DamageRelations = ({ damages }) => {
       });
     }, {});
 
-    console.log(result);
+    return result;
   };
 
   const separateObjectBetweenToAndFrom = (damage) => {
