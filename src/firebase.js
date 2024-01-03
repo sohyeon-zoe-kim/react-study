67c9893af16232da9f959578d87df7ea62a28c71
sohyeon-zoe-kim <hlna030838@gmail.com>
2024-01-03 20:30:38 +0900

    feat: firebase install 및 Navbar Layout 설정

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDJiaLxma3R1gID6nU4Sj_MZUe2HF-lYBY",
+  authDomain: "react-poke-app-32b77.firebaseapp.com",
+  projectId: "react-poke-app-32b77",
+  storageBucket: "react-poke-app-32b77.appspot.com",
+  messagingSenderId: "392133056624",
+  appId: "1:392133056624:web:67fbd4577709cfff857cb5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
