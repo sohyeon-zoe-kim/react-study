9c802cd9c2d12aa40195cd1925e742b8f87cb993
sohyeon-zoe-kim <hlna030838@gmail.com>
2024-01-03 20:52:37 +0900

    feat: styled-components 설치 및 navBar UI 스타일링

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,63 @@
+import styled from "styled-components";
+import { pokemonImg } from "../constants/url";
+import { useEffect, useState } from "react";
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const [show, setShow] = useState(false);
+  const listener = () => {
+    if (window.scrollY > 50) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listener);
+    return () => {
+      window.removeEventListener("scroll", listener);
+    };
+  }, []);
+
+  return (
+    <NavWrapper show={show}>
+      <Logo>
+        <Image
+          alt="Poke Logo"
+          src={`${pokemonImg}/25.png`}
+          onClick={() => {
+            window.location.href = "/";
+          }}
+        />
+      </Logo>
+    </NavWrapper>
+  );
 };
+
+const NavWrapper = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  display: flex;
+  background-color: ${(props) => (props.show ? "pink" : "transparent")};
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 100;
+`;
+
+const Image = styled.img`
+  cursor: pointer;
+  width: 100%;
+`;
+
+const Logo = styled.a`
+  padding: 0;
+  width: 50px;
+  margin-top: 4px;
+`;
 
 export default NavBar;
