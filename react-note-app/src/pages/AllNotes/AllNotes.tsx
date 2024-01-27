693101583b3dc8cdc1f7dccd18ce5241836d73ec
sohyeon-zoe-kim <hlna030838@gmail.com>
2024-01-27 18:13:11 +0900

    feat(react-note-app): Note 페이지 생성

--- react-note-app/src/pages/AllNotes/AllNotes.tsx
+++ react-note-app/src/pages/AllNotes/AllNotes.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useAppDisPatch, useAppSelector } from "../../hooks/redux";
+import { ButtonOutline, Container, EmptyMsgBox } from "../../styles/styles";
+import { Box, InputBox, TopBox } from "./AllNotes.styles";
+import { toggleFiltersModal } from "../../store/modal/modalSlice";
 
 const AllNotes = () => {
-  return (
-    <div>AllNotes</div>
-  )
-}
+  const dispatch = useAppDisPatch();
+  const { mainNotes } = useAppSelector((state) => state.nodeList);
+  const [filter, setFilter] = useState("");
+  const [searchInput, setSearchInput] = useState("");
 
-export default AllNotes
+  return (
+    <Container>
+      {mainNotes.length === 0 ? (
+        <EmptyMsgBox>노트가 없습니다.</EmptyMsgBox>
+      ) : (
+        <>
+          <TopBox>
+            <InputBox>
+              <input
+                type="text"
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.target.value)}
+                placeholder="노트의 제목을 입력해주세요."
+              />
+            </InputBox>
+            <div className="notes__filter-btn">
+              <ButtonOutline
+                className="nav__btn"
+                onClick={() => dispatch(toggleFiltersModal(true))}
+              >
+                <span>정렬</span>
+              </ButtonOutline>
+            </div>
+          </TopBox>
+          <Box></Box>
+        </>
+      )}
+    </Container>
+  );
+};
+
+export default AllNotes;
